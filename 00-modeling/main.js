639055acb55b9f4face3e8d8c1127e529fd7546f
Barry May <5514034+barrymay@users.noreply.github.com>
2022-02-16 03:39:40 +0000

    Changes for exercise 00 and 01

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -6,3 +6,53 @@ import '../style.css';
 
 // Also, come up with a simple way to "interpret" it, and
 // make it an object that you can `.send(...)` events to.
+
+
+const machine = {
+    initial: 'loading',
+    states: {
+        loading: {
+            on: {
+                LOADED: 'playing'
+            }
+        },
+        playing: {
+            on: {
+                PAUSE: 'paused'
+            }
+        },
+        paused: {
+            on: {
+                PLAY: 'playing'
+            }
+        }
+    }
+}
+
+let activeState = {
+    state: machine.initial,
+    send: function(event) {
+        transition(this, event)
+    }
+}
+
+
+
+function transition(state = {
+    state: machine.initial
+}, event) {
+    const machineState = machine.states[activeState.state];
+    const nextState = machineState?.on?.[event?.event];
+    if (nextState) {
+        activeState = {
+            ...activeState,
+            state: nextState
+        }
+    }
+    console.log(`STATE IS ${activeState.state}`);
+
+}
+
+
+
+window.activeState = activeState;
--- 01-states-transitions/main.js
+++ 01-states-transitions/main.js
@@ -5,12 +5,37 @@ import elements from '../utils/elements';
 
 import { inspect } from '@xstate/inspect';
 
-// inspect({
-//   iframe: false,
-//   url: 'https://stately.ai/viz?inspect',
-// });
+inspect({
+  iframe: false,
+  url: 'https://stately.ai/viz?inspect',
+});
 
-const playerMachine = createMachine({});
+const playerMachine = createMachine({
+  initial: "loading",
+  states: {
+    loading: {
+      on: {
+        LOADED: {
+          target: "playing",
+        }
+      },
+    },
+    playing: {
+      on: {
+        PAUSE: {
+          target:  "paused"
+        },
+      },
+    },
+    paused: {
+      on: {
+        PLAY: {
+          target: "playing"
+        } ,
+      },
+    },
+  },
+});
 
 const service = interpret(playerMachine, { devTools: true }).start();
 
